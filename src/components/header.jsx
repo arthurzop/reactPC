c7165c9289944bdfac933878f2a71401bca0e237
arthurzop <medeirosartur48@gmail.com>
2023-12-13 02:56:24 -0300

    startando

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./components.css"
+
+export const Header = () => {
+  return (
+    <>
+    <div className="header-container">
+        <h1>preto</h1>
+        <div className="header-links">
+            <h2>endereco</h2>
+            <h2>·</h2>
+            <h2>menu</h2>
+            <h2>reservar</h2>
+        </div>
+    </div>
+    </>
+  )
+}
